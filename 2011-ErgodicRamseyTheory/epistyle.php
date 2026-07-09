d14e438a4be015bf585b15c7921a914edb0c01d0
Laurentiu Leustean <lleustean@gmail.com>
2026-07-09 20:17:28 +0300

    Add files via upload

---
+++ 2011-ErgodicRamseyTheory/epistyle.php
@@ -0,0 +1,33 @@
+a:active, a:link, a:visited		{color: rgb(255, 102, 0); font-weight: bold; font-family: Verdana, Helvetica, Arial, Geneva, Swiss, SunSans-Regular, sans-serif; text-decoration: none }
+a:hover	{color: #336699; font-weight: bold; font-family: Verdana, Helvetica, Arial, Geneva, Swiss, SunSans-Regular, sans-serif; text-decoration: none }
+
+body 			{ color: rgb(102, 102, 102)}
+p			{ color: rgb(102, 102, 102); font-size: 16px; font-family: Verdana, Helvetica, Arial, Geneva, Swiss, SunSans-Regular, sans-serif }
+td			{ color: rgb(102, 102, 102); font-size: 16px; font-family: Verdana, Helvetica, Arial, Geneva, Swiss, SunSans-Regular, sans-serif }
+th			{ color: rgb(102, 102, 102); font-weight: bold; font-size: 16px; font-family: Verdana, Helvetica, Arial, Geneva, Swiss, SunSans-Regular, sans-serif; text-align: center }
+h1                      { color: #555555; font-size: 1.2em; font-weight: bold; margin: 0; padding-top: 4px; padding-bottom: 4px; }
+h2                      { color: red; font-size: 1.2em; font-weight: bold; margin: 0; padding-top: 4px; padding-bottom: 4px }
+h3                      { color: #555555; font-size: 1.2em; font-weight: bold; margin: 0; padding-top: 4px; padding-bottom: 4px; clear: both }
+
+.text  		{ color: rgb(102, 102, 102); font-size: 16px; font-family: Verdana, Helvetica, Arial, Geneva, Swiss, SunSans-Regular, sans-serif }
+.textbold  		{ color: rgb(102, 102, 102); font-weight: bold; font-size: 16px; font-family: Verdana, Helvetica, Arial, Geneva, Swiss, SunSans-Regular, sans-serif }
+.boxtext    	{ color: rgb(102, 102, 102); font-size: 16px; font-family: Verdana, Helvetica, Arial, Geneva, Swiss, SunSans-Regular, sans-serif; padding: 4px }
+.boxtextbold    	 { color: black; font-weight: bold; font-size: 16px; font-family: Verdana, Helvetica, Arial, Geneva, Swiss, SunSans-Regular, sans-serif; padding: 4px }
+.path, .path:active, .path:link, .path:visited, .path:hover	 { color: #666666; font-size: 16px; font-family: Verdana, Helvetica, Arial, Geneva, Swiss, SunSans-Regular, sans-serif }
+.title, .textgray  { color: #999999; font-weight: bold; font-size: 16px; font-family: Verdana, Helvetica, Arial, Geneva, Swiss, SunSans-Regular, sans-serif }
+.texttitle  	{ color: rgb(102, 102, 102); font-weight: bold; font-size: 16px; font-family: Verdana, Helvetica, Arial, Geneva, Swiss, SunSans-Regular, sans-serif }
+.boxtitle 		{ color: #FFFFFF; font-size: 16px; font-weight: bold; font-family: Verdana, Helvetica, Arial, Geneva, Swiss, SunSans-Regular, sans-serif }
+.texttitlewhtbig	{ color: #FFFFFF; font-weight: bold; font-size: 24px; font-family: Arial, Helvetica, Geneva, Swiss, SunSans-Regular, sans-serif }
+.texttitlewht	{ color: #FFFFFF; font-weight: bold; font-size: 14px; font-family: Arial, Helvetica, Geneva, Swiss, SunSans-Regular, sans-serif }
+.titlered   	{ color: #ff0000; font-weight: bold; font-size: 16px; font-family: Verdana, Helvetica, Arial, Geneva, Swiss, SunSans-Regular, sans-serif;}
+.linkedit, .linkedit:active, .linkedit:link, .linkedit:visited { color: #006633; font-size: 16px; font-weight: bold; font-family: Verdana, Helvetica, Arial, Geneva, Swiss, SunSans-Regular, sans-serif; text-decoration: none }
+.linkedit:hover  { color: #339933; font-size: 16px; font-weight: bold; font-family: Verdana, Helvetica, Arial, Geneva, Swiss, SunSans-Regular, sans-serif; text-decoration: none }
+
+.box			{ background-color: white; margin: 0 0 5px; padding: 4px; border-style: solid; border-width: 0 1px 1px }
+.box_title        { font-size: 1.2em; font-family: Helvetica, Arial, Verdana, sans-serif; font-weight: bold; text-transform: uppercase; text-align: left; padding-left: 5px; border-top: 2px solid; border-bottom: 1px solid }
+.red	       		{ color: white; background-color: red;  border-color: red }
+.grey	       		{ color: white;  background-color: #acacac; border-color: #acacac  }
+.white				{ color: #999999;  background-color: white; border-color: white  }
+.red_border			{ border-color: red }
+.grey_border		{ border-color: #acacac }
+.white_border		{ border-color: white }
